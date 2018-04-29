33ccae6d621ae3942f4f7d394f1663187f53e060
foxvac <38643902+foxvac@users.noreply.github.com>
2018-04-30 00:39:12 +0800

    Update app.js

--- static/app.js
+++ static/app.js
@@ -689,8 +689,8 @@ const renderMap = () => {
      }
     }
     if (playerObj.health != null) {
-      label += `|血:${Math.floor(playerObj.health)}|`
-    }
+      label += `|@:${Math.floor(playerObj.health)}|`
+    }    
     feature.set('_label', label)
     // re-add should be fine
     playerSource.addFeature(feature)
